2b01954b43635e47977bb4e1d96371541c266af5
nwarre <121927267+Niddish@users.noreply.github.com>
2023-11-08 16:08:04 -0500

    Create getinformation.py

---
+++ getinformation.py
@@ -0,0 +1,21 @@
+#creates CSV file of S&P 500 data and imports it into a pandas dataframe
+import yfinance as yf
+import pandas as pd
+import os
+
+if os.path.exists("sp500.csv"):
+    sp500 = pd.read_csv("sp500.csv", index_col=0)
+else:
+    sp500 = yf.Ticker("^GSPC")
+    sp500 = sp500.history(period="max")
+    sp500.to_csv("sp500.csv")
+
+sp500.index = pd.to_datetime(sp500.index, utc=True)
+
+del sp500["Dividends"]
+del sp500["Stock Splits"]
+sp500["Tomorrow"] = sp500["Close"].shift(-1)
+sp500["Target"] = (sp500["Tomorrow"] > sp500["Close"]).astype(int)
+sp500 = sp500.loc["1990-01-01":].copy()
+
+sp500
